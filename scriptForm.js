6d56c448d79e9b0b8f012dba9e87707de2668740
MelRecalde <melquisedicrecalde@gmail.com>
2024-06-10 23:30:57 +0200

    trying to fix the function

--- scriptForm.js
+++ scriptForm.js
@@ -1,3 +1,26 @@
+/*
+const form = document.querySelector("form")
+
+form.onsubmit = function (e) {
+    e.preventDefault()
+    form.reset()
+}*/
+
+let petNameField = document.getElementById('petName')
+let ownerNameField = document.getElementById('owner')
+let breedField = document.getElementById('breed')
+let speciesField = document.getElementById('species')
+/*
+let speciesDog = document.getElementById('dog')
+let speciesCat = document.getElementById('cat')
+let speciesRabbit = document.getElementById('rabbit')
+let speciesOthers = document.getElementById('other')
+*/
+let sendButton = document.getElementById('btn')
+let petList = document.getElementById('petList')
+
+let pets = []
+
 class Pet {
     constructor(petName, ownerName, species, breed) {
         this.petName = petName;
@@ -5,16 +28,40 @@ class Pet {
         this.species = species;
         this.breed = breed;
     }
+    checkOwner(otherPet) {
+        if (this.ownerName === otherPet.ownerName) {
+            return `${this.petName} and ${otherPet.petName} have the same owner`
+        } else {
+            return false
+        }
+    }
 }
 
 
-    const form = document.querySelector("form")
+const addList = function () {
+    petList.innerHTML = ''
+    pets.forEach((pet) => {
+      const newList = document.createElement('li')
+      newList.innerText =
+        'PET NAME: ' + pet.petName + ', OWNER: ' + pet.ownerName
+      petList.appendChild(newList)
+    })
+  }
 
-form.onsubmit = function (e) {
-        e.preventDefault()
+  sendButton.onclick = function () {
+    let newPet = new Pet(
+      petNameField.value,
+      ownerNameField.value,
+      speciesField.value,
+      breedField.value
+    )
+    
+    pets.push(newPet)
 
-
-        if ()
-
-            form.reset()
-    }
+    renderList()
+    petNameField.value = '' 
+    ownerNameField.value = '' 
+    breedField.value = '' 
+  }
+  
+  
